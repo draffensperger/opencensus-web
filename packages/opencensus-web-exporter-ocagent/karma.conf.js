501b43275527c3cbfcb74faffe632dca6995cb3f
draffensperger <daveraff@google.com>
2018-12-19 20:49:28 -0500

    Progress on getting files in

---
+++ packages/opencensus-web-exporter-ocagent/karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine', 'karma-typescript'],
+    files: [
+      {pattern: 'src/**/*.ts'},
+      {pattern: 'test/**/*.ts'},
+    ],
+    preprocessors: {
+      'src/**/*.ts': ['karma-typescript', 'coverage'],
+      'test/**/*.ts': ['karma-typescript'],
+    },
+    karmaTypescriptConfig: {
+      'extends': './tsconfig.json',
+    },
+    reporters: ['progress', 'coverage', 'karma-typescript'],
+    browsers: ['ChromeHeadless'],
+  });
+};
